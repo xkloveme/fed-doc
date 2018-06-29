523fd98532b5fe731d6c97a84c5f6e21c1079996
nashaofu <diaocheng@outlook.com>
2018-06-29 19:46:37 +0800

    配置地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,10 +6,22 @@ module.exports = {
       {
         text: '文档资源',
         items: [
-          { text: 'hekr-cli', link: '/hekr-cli/index.html' },
-          { text: 'hekr-h5-sdk', link: '/hekr-h5-sdk/index.html' },
-          { text: 'hekr-components', link: '/hekr-components/index.html' },
-          { text: 'auto', link: '/auto/index.html' }
+          {
+            text: 'hekr-cli',
+            link: 'http://10.10.1.3:7777/hekr-cli/index.html'
+          },
+          {
+            text: 'hekr-h5-sdk',
+            link: 'http://10.10.1.3:7777/hekr-h5-sdk/index.html'
+          },
+          {
+            text: 'hekr-components',
+            link: 'http://10.10.1.3:7777/hekr-components/index.html'
+          },
+          {
+            text: 'auto',
+            link: 'http://10.10.1.3:7777/auto/index.html'
+          }
         ]
       }
     ],
